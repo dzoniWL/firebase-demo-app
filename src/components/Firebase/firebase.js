57b0abfbd3234bb811f28afdc077c8da5218c60b
nikola veljovic <nikola.veljovic@sap.com>
2020-02-21 14:38:51 +0100

    add Firebase and React connenction

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,7 @@
+import app from 'firebase/app'
+
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API-KEY,
+    apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_DATABASE_URL,
     projectId: process.env.REACT_APP_PROJECT_ID,
@@ -7,4 +9,13 @@ const firebaseConfig = {
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_ID,
     measurementId: process.env.REACT_APP_MESUREMENT_ID
-};
+}
+
+class Firebase {
+    constructor(){
+        app.initializeApp(firebaseConfig)
+    }
+}
+
+export default Firebase
+
